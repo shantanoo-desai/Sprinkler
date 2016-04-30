831ba66e9ab9e7a75c130e5fb74a86836398e612
anrosent <anson.rosenthal@gmail.com>
2016-04-30 16:30:27 -0400

    Changes to make this work!
    - Don't need to bind the server socket
    - Instantiate an LtDecoder for the Receiver, use the 'consume_block' API
    - Pass file-like BytesIO wrapper around received data so we can parse it
      into an LT block using the existing stream API
    - Print the decoded file when done!

--- Receiver.py
+++ Receiver.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 from sys import argv, exit, stdin
 import os, time
 from twinSocket import *
@@ -15,16 +16,26 @@ def receiver():
 	recvSocket = twinSocket()
 	recvSocket.bindTheSock()
 
+	# Instantiate an LtDecoder 
+	decoder = decode.LtDecoder()
+
 	while True:
 		# Data received from the server and/or other clients
 		dataRecv, fromSource = recvSocket.recvFromSock(65535)
 
 		print("-----------------------------\n")
 		print("data received from: ", fromSource)
-		
-		
-		decode.run(dataRecv)
 
+		  # Kinda hacky, read a single block as a one-element sequence of blocks using the BytesIO file-like interface for bytes
+		lt_block = next(decode.read_blocks(BytesIO(dataRecv)))
+
+		# Feed the block to the decoder, returns true if decoding is complete
+		if decoder.consume_block(lt_block):
+		    print("Decoded file")
+
+		    # Extract the decoded file and print
+		    print(decoder.bytes_dump())
+		    exit(0)
 		
 	
 	recvSocket.closeSock()
