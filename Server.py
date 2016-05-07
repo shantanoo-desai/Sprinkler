cfb5b33ce07155628b9f5e5b922a315578e0a40d
Shan <shantanoo_desai@hotmail.com>
2016-05-07 23:10:25 +0200

    changes tabs to spaces acc. to PEP8

--- Server.py
+++ Server.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3.4
-
+#!/usr/bin/python3
 from sys import argv, exit
 import os, time
 from twinSocket import *
@@ -11,58 +10,58 @@ stdBlockSize = 1470
 
 def server(PATH, FILENAME):
 
-	print("Starting Server Mode\n")
-	
-	#------------PATH CHECK ------------------
-	pathFLAG = os.path.exists(PATH)
+    print("Starting Server Mode\n")
+    
+    #------------PATH CHECK ------------------
+    pathFLAG = os.path.exists(PATH)
 
-	if not pathFLAG:
-		print("Mentioned path does not exist. Check Again\n")
-		exit(1)
-	else:
-		print("Path exists.. checking file..\n")
-		# change into the given path
-		os.chdir(PATH)
+    if not pathFLAG:
+        print("Mentioned path does not exist. Check Again\n")
+        exit(1)
+    else:
+        print("Path exists.. checking file..\n")
+        # change into the given path
+        os.chdir(PATH)
 
-	#------------ FILE CHECK -----------------
-	fileFlag = os.path.isfile(FILENAME)
+    #------------ FILE CHECK -----------------
+    fileFlag = os.path.isfile(FILENAME)
 
-	if not fileFlag:
-		print("Mentioned File does not exist. Check Again\n")
-		exit(1)
-	else:
-		print("File exists..\n")
+    if not fileFlag:
+        print("Mentioned File does not exist. Check Again\n")
+        exit(1)
+    else:
+        print("File exists..\n")
 
-	# Intel HEXCODE (.ihex) used here
-	
-	with open(FILENAME, 'rb') as f:
+    # Intel HEXCODE (.ihex) used here
+    
+    with open(FILENAME, 'rb') as f:
 
-		sizeFile, Blocks = encode._split_file(f, stdBlockSize)
-		print("size of file is (Bytes): ", sizeFile)
-		print ("number of Blocks: ", len(Blocks))
-		packetCounter = 0
+        sizeFile, Blocks = encode._split_file(f, stdBlockSize)
+        print("size of file is (Bytes): ", sizeFile)
+        print ("number of Blocks: ", len(Blocks))
+        packetCounter = 0
 
-	# -------------- Socket for Sending 
-	with open(FILENAME, 'rb') as f:
-	    servSocket = twinSocket()
+    # -------------- Socket for Sending 
+    with open(FILENAME, 'rb') as f:
+        servSocket = twinSocket()
 
-	    while True:
-	    	print("Starting Fountain \n")
-	    	for each in encode.encoder(f, stdBlockSize):
-	    		try:
-	    			servSocket.sendToSock(each, MCASTGRP)
-	    			packetCounter += 1
-	    			if packetCounter not in range(3*len(Blocks)):
-	    				print("Packets Sent: ", packetCounter)
-	    				print("Closing Fountain")
-	    				break
-	    		except (socket.error,OSError) as e:
-	    			print("Error in Socket Sending procedure..")
-	    			exit(1)
-	    	break
+        while True:
+            print("Starting Fountain \n")
+            for each in encode.encoder(f, stdBlockSize):
+                try:
+                    servSocket.sendToSock(each, MCASTGRP)
+                    packetCounter += 1
+                    if packetCounter not in range(3*len(Blocks)):
+                        print("Packets Sent: ", packetCounter)
+                        print("Closing Fountain")
+                        break
+                except (socket.error,OSError) as e:
+                    print("Error in Socket Sending procedure..")
+                    exit(1)
+            break
 
-	servSocket.closeSock()
+    servSocket.closeSock()
 
 if __name__ == '__main__':
-	server(PATH='/home/testbed/hexfiles', FILENAME='tmpimage.ihex')
-	#server(PATH='/tmp/', FILENAME='myTry.tar.gz')
+    server(PATH='/home/testbed/hexfiles', FILENAME='tmpimage.ihex')
+    #server(PATH='/tmp/', FILENAME='myTry.tar.gz')
--- twinSocket.py
+++ twinSocket.py
@@ -13,65 +13,65 @@ MCASTPORT = 30001
 MTTL = 1
 
 class twinSocket(object):
-	"""Class for Socket Creation and Binding and also Sending and Receiving data.."""
-	def __init__(self, sock = None):
-		"""Create a socket"""
-					
-		try:
-			# creating a socket
-			sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-			# this is Optional --> multiuse of the Socket
-			#sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-			print("SOCKET CREATED....")
+    """Class for Socket Creation and Binding and also Sending and Receiving data.."""
+    def __init__(self, sock = None):
+        """Create a socket"""
+                    
+        try:
+            # creating a socket
+            sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+            # this is Optional --> multiuse of the Socket
+            #sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            print("SOCKET CREATED....")
 
-		except socket.error as e:
-			print("SOCKET CREATION FAILED.. ERROR CODE: " + str(e[0]) + "Message: " + e[1])
-			exit()
+        except socket.error as e:
+            print("SOCKET CREATION FAILED.. ERROR CODE: " + str(e[0]) + "Message: " + e[1])
+            exit()
 
-		else:
-			# assign the socket if already created
-			self.sock = sock
+        else:
+            # assign the socket if already created
+            self.sock = sock
 
-	def bindTheSock(self, host = '', port = MCASTPORT):
-		"""Bind the Socket for Multicasting"""
-		
-		try:
-			self.sock.bind((host, port))
+    def bindTheSock(self, host = '', port = MCASTPORT):
+        """Bind the Socket for Multicasting"""
+        
+        try:
+            self.sock.bind((host, port))
 
-				# Pack binary for joining the group
-			group_bin = socket.inet_pton(socket.AF_INET6, MCASTGRP)
-				# create a membership request by packing 0
-			mreq = group_bin + pack('@I', 0)
-				# join the Multicast Group
-			self.sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
-				# do not hear "self messages" on Loopback interface
-				# pack zero to stop loopback
-			self.sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, pack('@i', 0))
-				# to increase reach of the LL multicasting --> increase MTTL value
-			self.sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, pack('@I', MTTL))
-			print("SOCKET BINDED....")
+                # Pack binary for joining the group
+            group_bin = socket.inet_pton(socket.AF_INET6, MCASTGRP)
+                # create a membership request by packing 0
+            mreq = group_bin + pack('@I', 0)
+                # join the Multicast Group
+            self.sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+                # do not hear "self messages" on Loopback interface
+                # pack zero to stop loopback
+            self.sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, pack('@i', 0))
+                # to increase reach of the LL multicasting --> increase MTTL value
+            self.sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, pack('@I', MTTL))
+            print("SOCKET BINDED....")
 
-		except socket.error as e:
-			print("SOCKET BINDING FAILED.. ERROR CODE: " + str(e[0]) + "Message: " + e[1])
-			exit()
+        except socket.error as e:
+            print("SOCKET BINDING FAILED.. ERROR CODE: " + str(e[0]) + "Message: " + e[1])
+            exit()
 
-	def sendToSock(self, message, host = MCASTGRP, port = MCASTPORT):
-		"""Class Function to send data over the Socket.."""
-		try:
-			self.sock.sendto(message, (host, port))
-		except socket.error as e:
-			print("SENDING FAILED.. ERROR CODE: " + str(e[0]) + "Message: " + e[1])
-			exit()
+    def sendToSock(self, message, host = MCASTGRP, port = MCASTPORT):
+        """Class Function to send data over the Socket.."""
+        try:
+            self.sock.sendto(message, (host, port))
+        except socket.error as e:
+            print("SENDING FAILED.. ERROR CODE: " + str(e[0]) + "Message: " + e[1])
+            exit()
 
-	def recvFromSock(self, buffvalue):
-		"""Class Function to receive data over the Socket.."""
-		# we receive information in the form of tuple with tuple[0] = dataRec and tuple[1] = tuple of sender
-		gotSomething, whereFrom = self.sock.recvfrom(buffvalue)
-		source = whereFrom[0]
+    def recvFromSock(self, buffvalue):
+        """Class Function to receive data over the Socket.."""
+        # we receive information in the form of tuple with tuple[0] = dataRec and tuple[1] = tuple of sender
+        gotSomething, whereFrom = self.sock.recvfrom(buffvalue)
+        source = whereFrom[0]
 
-		# return a Tuple in the same form but more clearly: Data, LL-IPv6-Address 
-		return gotSomething, source
+        # return a Tuple in the same form but more clearly: Data, LL-IPv6-Address 
+        return gotSomething, source
 
-	def closeSock(self):
-		print("CLOSING SOCKET..")
-		self.sock.close()
+    def closeSock(self):
+        print("CLOSING SOCKET..")
+        self.sock.close()
